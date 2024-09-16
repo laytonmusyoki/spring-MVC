fb94a8af4e345b4929e1daeb4e48044aa6ba02fe
layton <laytonmatheka@gmail.com>
2024-09-16 23:30:23 +0300

    initial commits

---
+++ src/main/java/com/authentication/MyStore/MyStoreApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.MyStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStoreApplication.class, args);
+	}
+
+}
